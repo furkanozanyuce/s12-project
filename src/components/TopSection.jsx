5c364221dcab443ccf4c8bd07c1cb701c11003a2
furkanozanyuce <furkanozanyuce@gmail.com>
2024-12-01 09:35:02 +0300

    created a languages data for future use

--- src/components/TopSection.jsx
+++ src/components/TopSection.jsx
@@ -1,3 +1,5 @@
+import langugaesData from "../languages";
+
 
 function TopSection() {
 
---
+++ src/languages.js
@@ -0,0 +1,22 @@
+const langugaesData = {
+    en: {
+        hero: {
+            name: "Name",
+            title: "I am a Frontend Developer...",
+            about: "...who likes to craft solid and scalable frontend products with great user experiences.",
+            langButton: "TÜRKÇE'YE GEÇ",
+            modeButton: "DARK MODE",
+        },
+    },
+    tr: {
+        hero: {
+            name: "İsim",
+            title: "Ben bir Frontend Developer'ım...",
+            about: "harika kullanıcı deneyimiyle sağlam ve ayarlanabilir frontend ürünleri tasarlarım",
+            langButton: "FOR ENGLISH",
+            modeButton: "GECE MODU",
+        }
+    }
+}
+
+export default langugaesData;
