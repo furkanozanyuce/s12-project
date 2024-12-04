17849e05de8d37d1960db0645464f1041f80b294
furkanozanyuce <furkanozanyuce@gmail.com>
2024-12-04 14:01:48 +0300

    small adjustments

--- src/components/TopSection.jsx
+++ src/components/TopSection.jsx
@@ -28,8 +28,8 @@ function TopSection() {
                     <h1 className="text-primary">{actualLang.title}</h1>
                     <h2>{actualLang.about}</h2>
                     <div className="button-container">
-                        <button className="actual-button bg-white text-secondary border-solid border-2 border-white dark:bg-[#252128] dark:text-white"> <img src="./public/github.svg"/> Github</button>
-                        <button className="actual-button bg-white text-secondary border-solid border-2 border-white dark:bg-[#252128] dark:text-white"> <img src="./public/linkedin.svg"/> Linkedin</button>
+                        <button className="actual-button bg-white text-secondary border-solid border-2 border-white dark:bg-[#252128] dark:text-white"> <img src="/github.svg"/> Github</button>
+                        <button className="actual-button bg-white text-secondary border-solid border-2 border-white dark:bg-[#252128] dark:text-white"> <img src="/linkedin.svg"/> Linkedin</button>
                     </div>
                 </div>
                 <img className="main-image" src="https://s3-alpha-sig.figma.com/img/76b0/6993/b04fd1fa29a883e2f4b8b8577a5c82eb?Expires=1733702400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=G~AogFFfiA3Yl2GccYd4A267KmgCGncPIJRrpixm4xV9Zn6hfXm1UUvyHvJ6lkKrW3u0FE2ECSHssuPYhB1taepp57vJXsCIRjNdnaA7g1VkWv3wXOKdGAdqAZlpZsYSQ6wj3d0F-qquGprCPpuv3FaxJzRS7uVUM1j4StUlmC-7eJxJD2K7Rzps6jomuZ83XJ7JZ1vzuXr~DWckF~m76VYctm8-47B1zPjyApgUZDxRbrSAML6CCWrpmkXXRZy9kfodrWvrUj-2Vta8VbC7xmksoszY6EJtoPVnEqwEeNkpKbavM-DoLsFwxsE7ROzW3uozCRlv70pClFdrrvQYgg__" />
